9e327a86d1cbfc3b4e0d273751e019c903c1794f
shyam <shyam20998@gmail.com>
2024-12-04 18:32:29 +0530

    updated movie poster UI

--- client/src/components/MoviePoster/MoviePoster.js
+++ client/src/components/MoviePoster/MoviePoster.js
@@ -1,69 +1,59 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Wrapper } from "./MoviePoster.styled";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import InfoIcon from "@mui/icons-material/Info";
 import { useAppContext } from "../../context/AppContext";
-import { poster_url } from "../../Requests";
-import { Skeleton } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import AddIcon from "@mui/icons-material/Add";
+import ImageComponent from "../ImageComponent";
 
 const MoviePoster = ({ movie }) => {
 	const navigate = useNavigate();
 	const { fetchMovieVideos, updateWatchList, watchList } = useAppContext();
 	const [imageLoaded, setImageLoaded] = useState(false);
 
-	useEffect(() => {
-		const img = new Image();
-		img.onload = () => {
-			setImageLoaded(true);
-		};
-		img.src = `${poster_url}${movie.poster_path}`;
-	}, [movie.poster_path]);
-
 	return (
 		<>
 			{movie.poster_path && (
 				<>
 					<Wrapper isLoaded={imageLoaded}>
-						<div className="row__poster">
-							<div className="row__poster-img">
-								<Skeleton
-									variant="rectangular"
-									animation="wave"
-									height="100%"
-									width="100%"
-									sx={{ display: imageLoaded ? "none" : "block" }}
+						<div className="movie__poster">
+							<Link to={`/details/${movie.id}`}>
+								<ImageComponent
+									src={`${movie.poster_path}`}
+									alt={movie.original_title}
+									imageLoaded={imageLoaded}
+									setImageLoaded={setImageLoaded}
 								/>
-								<img
-									style={{ display: imageLoaded ? "block" : "none" }}
-									src={`${poster_url}${movie.poster_path}`}
-									alt={movie.id}
-									onClick={() => navigate(`/details/${movie.id}`)}
-									loading="lazy"
-									height="100%"
-									width="100%"
-								/>
-							</div>
+							</Link>
 							{imageLoaded && (
-								<div className="row__poster-action">
+								<div className="movie__poster-actions">
 									<button
 										className="round-button"
-										onClick={() => fetchMovieVideos(movie.id)}>
+										onClick={() =>
+											fetchMovieVideos(movie.id)
+										}>
 										<PlayArrowIcon />
 									</button>
 									<button
 										className="round-button"
-										onClick={() => navigate(`/details/${movie.id}`)}>
+										onClick={() =>
+											navigate(`/details/${movie.id}`)
+										}>
 										<InfoIcon />
 									</button>
 									<button
 										className="round-button"
 										onClick={() =>
-											updateWatchList(movie.id, movie.poster_path)
+											updateWatchList(
+												movie.id,
+												movie.poster_path
+											)
 										}>
-										{Object.keys(watchList).includes(`${movie.id}`) ? (
+										{Object.keys(watchList).includes(
+											`${movie.id}`
+										) ? (
 											<DoneIcon />
 										) : (
 											<AddIcon />
--- client/src/components/MoviePoster/MoviePoster.styled.js
+++ client/src/components/MoviePoster/MoviePoster.styled.js
@@ -1,33 +1,18 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-	.row__poster {
+	.movie__poster {
 		cursor: pointer;
 		position: relative;
 		transition: transform 450ms;
 		max-height: 300px;
 
 		&:hover {
-			transform: ${(props) => (props.isLoaded ? "scale(1.07)" : "none")};
+			transform: ${(props) => (props.isLoaded ? "scale(1.05)" : "none")};
 			z-index: 9;
 		}
 
-		&-img {
-			width: 180px;
-			height: 270px;
-
-			@media only screen and (max-width: 768px) {
-				width: 140px;
-				height: 210px;
-			}
-
-			img {
-				object-fit: contain;
-				border-radius: 10px;
-			}
-		}
-
-		&-action {
+		&-actions {
 			position: absolute;
 			bottom: 0;
 			background: black;
@@ -41,7 +26,7 @@ export const Wrapper = styled.div`
 			border-bottom-left-radius: 10px;
 		}
 
-		&:hover .row__poster-action {
+		&:hover .movie__poster-actions {
 			visibility: visible;
 		}
 	}
